cd8ef24dde1a9b02bea2300f60498d35ce5c8ddb
María Vázquez Sáez <mariav.s.00@gmail.com>
2021-03-19 13:11:38 +0100

    OrderService functions approach

--- src/main/java/coreapi/OrderService.java
+++ src/main/java/coreapi/OrderService.java
@@ -2,6 +2,9 @@ package coreapi;
 //Falta libreria para linkedHashMap
 public class OrderService
 {
+	
+	/*---------------------------ORDER_PRODUCTS------------------------------------------*/
+	
 	/*
 	 * PRECONDITION:Receive an order and an id of a existing product, plus a positive quantity 
 	 * POSTCONDITION: Add the product with the indicated quantity to the order
@@ -20,7 +23,7 @@ public class OrderService
 				 * We check that the quantity of the product to be introduced is valid, 
 				 * that is, that it is greater than 0 and that there is enough stock
 				 */
-				if(q > 0 && q < linkedHashMap.get(actual))
+				if(q > 0 && q < mapProdQuant.get(actual))
 				{
 					ord.addProduct(actual,q);
 				}
@@ -53,7 +56,69 @@ public class OrderService
 				 * that is, that it is greater than 0 and that there is enough stock
 				 */
 				
+				//Necesito acceder a la cantidad de producto que hay en la cesta
 				
 			}
+		}
+	
 	}
+	/*
+	 * PRECONDITION:Receive an order and an id of a existing product, plus a positive quantity 
+	 * POSTCONDITION:Eliminate the indicated amount of the product
+	 */
+	public void removeProductFromOrder(Order ord, int productId, int q)
+	{
+		//Necesito acceder a la cantidad del producto en la cesta
+	}
+	
+	/* ---------------------------------ORDER_STATUS----------------------------------*/
+	/*
+	 * PRECONDITION:Receive an order
+	 * POSTCONDITION: Assign the status to the order
+	 */
+	public void OrderStatus_InKitchen(Order ord)
+	{
+		//Debe haber productos en la lista
+	}
+	
+	/*
+	 * PRECONDITION:Receive an order
+	 * POSTCONDITION: Assign the status to the order
+	 */
+	public void OrderStatus_Delivered(Order ord)
+	{
+		//El estado debe ser en cocina para poder ser entregado
+	}
+	/*
+	 * PRECONDITION:Receive an order
+	 * POSTCONDITION: Assign the status to the order
+	 */
+	public void OrderStatus_Charged(Order ord)
+	{
+		//El estado debe de ser entregado o en cocina para poder ser cobrado
+	}
+	
+	/*
+	 * PRECONDITION:Receive an order
+	 * POSTCONDITION: Assign the status to the order
+	 */
+	public void OrderStatus_Finalized(Order ord)
+	{
+		//El estado debe ser cobrado para poder ser finalizada
+	}
+	
+	/*-------------------------------DAILY_REGISTER-------------------------------*/
+	
+	/*
+	 * PRECONDITION:Receive a date
+	 * POSTCONDITION: Print on screen the number of orders and the total amount of 
+	 * all orders for the indicated date
+	 */
+	public void checkDailyRegister(Fecha fech)
+	{
+		//Necesito acceder a una lista de todos los pedidos
+	}
+	
+	
+	
 }
