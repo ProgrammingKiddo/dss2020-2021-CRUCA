929a8aed08fe9630f8ea63303b095b1591c3aa95
María Vázquez Sáez <mariav.s.00@gmail.com>
2021-03-22 21:10:02 +0100

    New method ProductResgister where we add a product with a specific stock to the Cafeteria

--- src/main/java/coreapi/Cafeteria.java
+++ src/main/java/coreapi/Cafeteria.java
@@ -3,14 +3,23 @@ package coreapi;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
-public class Cafeteria {
+public class Cafeteria 
+{
 
 	public LinkedHashMap<Product, Integer> productStock;
 	public ArrayList<Order> orderHistory;
 	public ArrayList<Order> dailyOrders;
+	public OrderFactory ordFact;
 	
 	Cafeteria()
 	{
 		productStock = new LinkedHashMap<Product, Integer>();
+		ordFact = new OrderFactory();
 	}
+	
+	public void ProductRegister(Product prod, int q)
+	{
+		productStock.put(prod,q);
+	}
+	
 }
