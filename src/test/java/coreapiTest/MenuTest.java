654f6f5253d6522477eeb4aff1dbab40460c09bb
ProgrammingKiddo <programming.edu@protonmail.com>
2021-03-29 02:33:46 +0200

    Fixed the tests of the Menu class.

--- src/test/java/coreapiTest/MenuTest.java
+++ src/test/java/coreapiTest/MenuTest.java
@@ -14,79 +14,60 @@ import coreapi.ProductCatalog;
 
 public class MenuTest
 {
-	private Menu menu1;
-	private Menu menu2;
-	private Menu menu3;
-	private Product p1;
-	private Product p2;
-	private Product p3;
+	private Menu myMenu;
+	private Product product1 = ProductCatalog.getProduct(0);
+	private Product product2 = ProductCatalog.getProduct(1);
 	
 	@Before
 	public void setUp()
 	{
-		menu1 = new Menu(1,"Oro");
-		menu2 = new Menu(2,"Plata");
-		menu3 = new Menu(3,"Bronce");
-		p1 = ProductCatalog.getProduct(0);
-		p2 = ProductCatalog.getProduct(1);
-		p3 = ProductCatalog.getProduct(2);
+		myMenu = new Menu(1, "Oro");
 	}
 	
 	@After
 	public void tearDown()
 	{
-		menu1 = null;
-		menu2 = null;
-		menu3 = null;
+		myMenu = null;
 	}
 	
 	@Test
 	public void addProductToMenuCheck()
 	{
-		menu1.addProductToMenu(p1,1);
-		menu1.addProductToMenu(p2,2);
-		menu1.addProductToMenu(p3,3);
-		Assert.assertTrue(menu1.getProductsInMenu().contains(p1));
-		Assert.assertTrue(menu1.getProductsInMenu().contains(p2));
-		Assert.assertTrue(menu1.getProductsInMenu().contains(p3));
+		myMenu.addProductToMenu(product1, 1);
+		myMenu.addProductToMenu(product2, 4);
+		Assert.assertTrue(myMenu.getProductsInMenu().contains(product1));
+		Assert.assertEquals(4, myMenu.getProductQuantity(product2.getId()));
 	}
 	
 	@Test
 	public void IdCheckMenu()
 	{
-		Assert.assertEquals("DifferentIdAtMenu1",1,menu1.getId());
-		Assert.assertEquals("DifferentIdAtMenu2",2,menu2.getId());
-		Assert.assertEquals("DifferentIdAtMenu3",3,menu3.getId());
+		Assert.assertEquals("DifferentIdAtMenu1", 1, myMenu.getId());
 	}
 	
 	@Test
 	public void NameCheckMenu()
 	{
-		Assert.assertEquals("DifferentNameAtMenu1","Oro",menu1.getName());
-		Assert.assertEquals("DifferentNameAtMenu2","Plata",menu2.getName());
-		Assert.assertEquals("DifferentNameAtMenu3","Bronce",menu3.getName());
+		Assert.assertEquals("DifferentNameAtMenu1", "Oro", myMenu.getName());
 	}
 	
 	@Test
 	public void PriceCheckMenu()
 	{
-		Assert.assertEquals("DifferentPriceAtMenu1",menu1.getName());
-		Assert.assertEquals("DifferentPriceAtMenu2",menu2.getName());
-		Assert.assertEquals("DifferentPriceAtMenu3",menu3.getName());
+		myMenu.addProductToMenu(product1, 2);
+		myMenu.addProductToMenu(product2, 4);
+		float expectedPrice = (product1.getPrice() * 2) + (product2.getPrice() * 4);
+		Assert.assertEquals(expectedPrice, myMenu.getPrice(), 0.0);
 	}
 	
 	@Test
 	public void GetProductsInMenuCheck()
 	{
-		menu2.addProductToMenu(p1,1);
-		menu2.addProductToMenu(p2,2);
-		menu2.addProductToMenu(p3,3);
-		int Comp = 0;
-		List<Product> MenuProducts = menu2.getProductsInMenu();
+		myMenu.addProductToMenu(product1);
+		myMenu.addProductToMenu(product2);
+		List<Product> MenuProducts = myMenu.getProductsInMenu();
 		
-		for(Product p : MenuProducts)
-		{
-			Assert.assertEquals("AddProductToMenuError",p.getId(),Comp);
-		}
+		Assert.assertTrue(MenuProducts.contains(product1));
+		Assert.assertTrue(MenuProducts.contains(product2));
 	}
 }
