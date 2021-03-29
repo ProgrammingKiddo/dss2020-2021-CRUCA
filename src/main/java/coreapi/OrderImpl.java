0aea45e1035309eef2b8cd95bada0366f5f42864
ProgrammingKiddo <programming.edu@protonmail.com>
2021-03-29 23:28:29 +0200

    Implemented the changes necessary to accomodate the use of BigDecimal to handle prices, and improved the javadoc documentation of OrderImpl and ProductImpl.

--- src/main/java/coreapi/OrderImpl.java
+++ src/main/java/coreapi/OrderImpl.java
@@ -1,5 +1,12 @@
+/**
+ * Implementation of the <code>Order</code> interface.
+ * Users of the API shouldn't use this class directly.
+ * @author Borja
+ * @version 0.2
+ */
 package coreapi;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,7 +22,11 @@ public class OrderImpl implements Order {
 	private LinkedHashMap<Product, Integer> basket;
 	private OrderStatus orderStatus;
 	
-	
+	/**
+	 * Creates a new <code>OrderImpl</code> instance.
+	 * @param assignedId the identifier assigned to this order.
+	 * @param creationDate the timestamp representing the moment this order was created.
+	 */
 	OrderImpl(int assignedId, Date creationDate)
 	{
 		id = assignedId;
@@ -24,26 +35,59 @@ public class OrderImpl implements Order {
 		orderStatus = OrderStatus.OPEN;
 	}
 	
+	/**
+	 * Returns the unique id assigned by the order factory to this specific order.
+	 * @return returns the id of this order.
+	 * @see OrderFactory
+	 */
 	public int getId() { return id; }
+	/**
+	 * Returns the timestamp representing the moment this order was created. 
+	 * @return the date on which the order was created.
+	 * @see Date
+	 */
 	public Date getDate() { return date; }
+	/**
+	 * Returns a read-only value of the current state of this order.
+	 * @return returns the state of this order.
+	 * @see OrderStatus
+	 */
 	public OrderStatus getStatus() { return OrderStatus.valueOf(orderStatus.name()); }
+	/**
+	 * Sets the current status of this order to the one passed by parameter.
+	 * @param newStatus the new status to set this order to.
+	 * @see OrderStatus
+	 */
 	public void setStatus(OrderStatus newStatus) { orderStatus = newStatus; }
 	
 	/**
-	 * @return Returns the total cost of all the products contained in the Order.
+	 * Returns the total cost of all the products contained in the order.
+	 * Takes into account the individual price of each product, as well as how many
+	 * of them are currently in this order.
+	 * @return the combined cost of this order.
+	 * @see BigDecimal
 	 */
-	public float totalCost() 
+	public BigDecimal totalCost() 
 	{
-		float sumCost = 0;
+		BigDecimal sumCost = BigDecimal.ZERO;
+		BigDecimal productCost = BigDecimal.ZERO;
+		BigDecimal productQuantity = BigDecimal.ZERO;
+		
 		for (Map.Entry<Product, Integer> entry : basket.entrySet())
 		{
-			sumCost += entry.getKey().getPrice() * entry.getValue().intValue();
+			// We create a copy of the product's price as to not modify its value via this reference.
+			productCost = entry.getKey().getPrice();
+			productQuantity = new BigDecimal(entry.getValue().intValue());
+			productCost = productCost.multiply(productQuantity);
+			
+			sumCost = sumCost.add(productCost);
 		}
 		return sumCost;
 	}
 	
 	/**
-	 * @return Returns a read-only map containing what products are in this Order.
+	 * Returns a read-only list containing what products are in this Order.
+	 * @return the list of products contained in this order.
 	 */
 	public List<Product> getProducts()
 	{
@@ -51,8 +95,9 @@ public class OrderImpl implements Order {
 	}
 	
 	/**
-	 * @return Returns a read-only map containing what products and how many
-	 * of them are in this Order.
+	 * Returns a read-only map containing what products and how many
+	 * of each of them are in this order.
+	 * @return the map of products and their quantities in this order.
 	 */
 	public Map<Product,Integer> getBasket()
 	{
@@ -60,7 +105,10 @@ public class OrderImpl implements Order {
 	}
 	
 	/**
-	 * @return Returns whether or not the Product determined by the id is contained in this Order.
+	 * Returns whether or not the product determined by the id is contained in this order.
+	 * Returns <code>true</code> if there is at least one unit of the product in this order, and
+	 * <code>false</code> if there is none.
+	 * @return if the product represented by the id exists in this order.
 	 */
 	public boolean containsProduct(int id)
 	{
@@ -68,7 +116,9 @@ public class OrderImpl implements Order {
 	}
 
 	/**
-	 * @return Returns how many existences of a Product are there in this Order.
+	 * Returns how many existences of the product determined by the id are there in this order.
+	 * Returns zero if the product in question isn't in this order.
+	 * @return the quantity of a product in this order.
 	 */
 	public int checkProductQuantity(int id)
 	{
@@ -84,12 +134,19 @@ public class OrderImpl implements Order {
 		}
 		return quantity;
 	}
-
+	/**
+	 * Adds a single unit of the product determined by the id to this order.
+	 * @param newProductId the id of the product to add.
+	 */
 	public void addProduct(int newProductId)
 	{
 		addProduct(newProductId, 1);
 	}
-	
+	/**
+	 * Adds a certain amount of units of the product determined by the id to this order.
+	 * @param newProductId the id of the product to add.
+	 * @param quantity the amount of the product to add.
+	 */
 	public void addProduct(int newProductId, int quantity)
 	{
 		if (quantity > 0)
@@ -106,12 +163,22 @@ public class OrderImpl implements Order {
 			}	
 		}
 	}
-	
+	/**
+	 * Removes all amounts of the product determined by the id from this order.
+	 * The product in question will no longer be contained within this order.
+	 * @param productId the id of the product to remove.
+	 */
 	public void removeProduct(int productId)
 	{
 		basket.remove(ProductCatalog.Instance().getProduct(productId));
 	}
-	
+	/**
+	 * Removes a certain amount of units of the product determined by the id from this order.
+	 * If the amount to remove is equal to or greater that the amount of that product currently
+	 * in the order, the product in question is removed from the order entirely.
+	 * @param productId the id of the product to remove.
+	 * @param quantity the amount of the product to remove.
+	 */
 	public void removeProduct(int productId, int quantity)
 	{
 		if (quantity > 0)
--- src/main/java/coreapi/ProductImpl.java
+++ src/main/java/coreapi/ProductImpl.java
@@ -1,22 +1,50 @@
+/**
+ * Implementation of the <code>Product</code> interface.
+ * Users of the API shouldn't use this class directly.
+ * @author Fran
+ * @author Borja
+ * @version 0.2
+ */
 package coreapi;
+
+import java.math.BigDecimal;
 
 public class ProductImpl implements Product {
 
 	private int id;
-	private float price;
+	private BigDecimal price;
 	private String name;
 	
-	
-	public ProductImpl(int id, float price, String name)
+	/**
+	 * Creates a new instance of a specific product.
+	 * To be used only in the creation of the product catalog.
+	 * @param id the identifier assigned to this product.
+	 * @param price the cost of this product.
+	 * @param name the name assigned to this product.
+	 * @see ProductCatalog
+	 */
+	public ProductImpl(int id, BigDecimal price, String name)
 	{
 		this.id = id;
 		this.price = price;
 		this.name = name;
 	}
-	
+	/**
+	 * Returns the unique id assigned by the product catalog to this specific catalog.
+	 * @return returns the id of this product.
+	 * @see ProductCatalog
+	 */
 	public int getId() { return id; }
-	
-	public float getPrice() { return price; }
-	
+	/**
+	 * Returns the price of this product as a <code>BigDecimal</code> to avoid precision loss.
+	 * @return returns the price of this product.
+	 * @see BigDecimal
+	 */
+	public BigDecimal getPrice() { return price; }
+	/**
+	 * Returns the name assigned by the product catalog to this specific catalog.
+	 * @return returns the name of this product.
+	 * @see ProductCatalog
+	 */
 	public String getName() {return name;}
 }
