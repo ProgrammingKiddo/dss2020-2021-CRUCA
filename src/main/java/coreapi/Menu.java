e427f5768a92eef94eff078fff8478f453da2085
ProgrammingKiddo <programming.edu@protonmail.com>
2021-03-29 02:18:44 +0200

    Added javadoc documentation and provided some fixes to the Menu class.

--- src/main/java/coreapi/Menu.java
+++ src/main/java/coreapi/Menu.java
@@ -2,10 +2,9 @@ package coreapi;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class Menu implements Product {
 	
@@ -13,6 +12,13 @@ public class Menu implements Product {
 	private int id;
 	private String name;
 	
+	/**
+	 * Creates a new menu with the assigned numerical Id and name.
+	 * The user of this class should be aware to assign a unique identifier to this menu,
+	 * and to provide a user-friendly name for display.
+	 * @param assignedId the numerical identifier to assign to this menu.
+	 * @param assignedName the name to assign to this menu.
+	 */
 	public Menu(int assignedId, String assignedName)
 	{
 		id = assignedId;
@@ -20,45 +26,76 @@ public class Menu implements Product {
 		productsMenu = new LinkedHashMap<Product, Integer>();
 	}
 	
-	/*
-	 * @return Returns the unique identifier assigned to this Menu.
+	/**
+	 * @return the identifier assigned to this menu.
 	 */
 	public int getId() { return id; }
-	/*
-	 * @return Returns the name of this Menu.
+	/**
+	 * @return the name of this menu.
 	 */
 	public String getName() { return name; }
 	
-	/*
-	 * @return Returns the combined cost of all products that make up this Menu.
+	/**
+	 * Returns the combined cost of all products that make up this menu.
+	 * @return the cost of the entire menu.
 	 */
-	public float  getPrice() {
-		
-		float price = 0;
-		Set<Product> containedProducts = productsMenu.keySet();
-		Iterator<Product> prodIterator = containedProducts.iterator();
-		
-		while (prodIterator.hasNext())
+	public float getPrice()
+	{	
+		float sumCost = 0;
+		for (Map.Entry<Product, Integer> entry : productsMenu.entrySet())
 		{
-			price += (prodIterator.next().getPrice() * productsMenu.get(prodIterator.next()));
+			sumCost += entry.getKey().getPrice() * entry.getValue().intValue();
 		}
-		return price;
+		return sumCost;
 	}
-	
-	/*
-	 * @return Returns a read-only list containing the products that make up this Menu.
+	/**
+	 * Returns a read-only list containing the products that make up this menu.
+	 * @return a list containing the products in the menu.
+	 * @see List
+	 * @see Product
 	 */
 	public List<Product> getProductsInMenu()
 	{
 		return Collections.unmodifiableList(new ArrayList<Product>(productsMenu.keySet()));
 	}
-	
-	/*
-	 * Add a product to the menu.
+	/**
+	 * Returns how much of a product is contained in this menu.
+	 * If the product doesn't exist within this menu, the returned value is zero.
+	 * @param productId the id of the product to check.
+	 * @return the quantity of the product contained in this menu.
 	 */
-	
-	public void addProductToMenu(Product product, Integer q)
+	public int getProductQuantity(int productId)
 	{
-		productsMenu.put(product,q);
+		Product productToCheck = ProductCatalog.getProduct(productId);
+		int productQuantity;
+		
+		if (productsMenu.containsKey(productToCheck))
+		{
+			productQuantity = productsMenu.get(ProductCatalog.getProduct(productId)).intValue();			
+		}
+		else
+		{
+			productQuantity = 0;
+		}
+		return productQuantity;
+	}
+	/**
+	 * Adds one unit of the specified product to the menu.
+	 * @param product the product to add to the menu.
+	 * @see Product
+	 */
+	public void addProductToMenu(Product product)
+	{
+		this.addProductToMenu(product, 1);
+	}
+	/**
+	 * Add the specified amount of units of  the specified product to the menu.
+	 * @param product the product to add to the menu.
+	 * @param quantity how much of that product to add to the menu.
+	 * @see Product
+	 */
+	public void addProductToMenu(Product product, int quantity)
+	{
+		productsMenu.put(product, Integer.valueOf(quantity));
 	}
 }
