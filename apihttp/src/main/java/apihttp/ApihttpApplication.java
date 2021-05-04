06cab1761d39633f1e29d84202dac63a86b1cb7d
ProgrammingKiddo <programming.edu@protonmail.com>
2021-05-04 16:49:16 +0200

    Created the skeleton for the Spring API HTTP project.

---
+++ apihttp/src/main/java/apihttp/ApihttpApplication.java
@@ -0,0 +1,13 @@
+package apihttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihttpApplication.class, args);
+	}
+
+}
---
+++ apihttp/src/test/java/apihttp/ApihttpApplicationTests.java
@@ -0,0 +1,13 @@
+package apihttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
