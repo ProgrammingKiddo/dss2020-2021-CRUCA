283a11bb949b4d5af1a93255b4cfd1b5dbf61096
ProgrammingKiddo <programming.edu@protonmail.com>
2021-05-05 18:38:46 +0200

    Included the necessary dependencies to use the coreapi in other projects.

--- apihttp/src/main/java/apihttp/OrderController.java
+++ apihttp/src/main/java/apihttp/OrderController.java
@@ -1,49 +1,89 @@
 package apihttp;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import coreapi.Cafeteria;
+import coreapi.InsufficientStockException;
+import coreapi.Order;
+import coreapi.OrderService;
+import coreapi.OrderStatus;
+import coreapi.ProductNotContainedInOrderException;
+import coreapi.UnreachableStatusException;
 
 @RestController
 public class OrderController {
 
 	private final OrderService OService;
 	
+<<<<<<< Updated upstream
 	public Orderhttp(OrderService os){
+=======
+	OrderController(OrderService os){
+>>>>>>> Stashed changes
 		this.OService = os;
 	}
 	
 	@PatchMapping("/orders/{parameters}")
 	public void addProductToOrder(@PathVariable("parameters") Cafeteria coffe, Order ord, int ProductId, int Quantity)
 	{
-		Oservice.addProductToOrder(coffe,ord,ProductId,Quantity);
+		try {
+			OService.addProductToOrder(coffe,ord,ProductId,Quantity);			
+		} catch (InsufficientStockException ex)
+		{
+			
+		}
 	}
 	
 	@DeleteMapping("/orders/{parameters}")
 	public void removeProductFromOrder(@PathVariable("parameters") Order ord, int ProductId, int Quantity)
 	{
-		Oservice.removeProductFromOrder(ord,ProductId,Quantity);
+		try {
+			OService.removeProductFromOrder(ord,ProductId,Quantity);		
+		} catch (InsufficientStockException ex)
+		{
+			
+		}
+		catch (ProductNotContainedInOrderException ex)
+		{
+			
+		}
 	}
 	
 	@PatchMapping("/orders/{parameters}")
 	public void changeStatus(@PathVariable("parameters") Order ord, OrderStatus status)
 	{
-		switch(status)
+		try {
+			switch(status)
+			{
+				case IN_KITCHEN:
+					OService.OrderStatus_InKitchen(ord);
+					break;
+				case DELIVERED:
+					OService.OrderStatus_Delivered(ord);
+					break;
+				case PAYED:
+					OService.OrderStatus_Payed(ord);
+					break;
+				case FINISHED:
+					OService.OrderStatus_Finished(ord);
+					break;
+				default:
+					break;
+			}
+		} catch (UnreachableStatusException ex)
 		{
-			case IN_KITCHEN:
-				OService.OrderStatus_InKitchen(ord);
-			case DELIVERED:
-				OService.OrderStatus_Delivered(ord);
-			case PAYED:
-				OService.OrderStatus_Payed(ord);
-			case FINISHED:
-				OService.OrderStatus_Finished(ord);
+			
 		}
 	}
 	
 	@GetMapping("/orders/{parameters}")
 	public String DailyRegister(@PathVariable("parameters") Cafeteria coffe, LocalDate date)
 	{
-		return "La caja de la fecha introducida es: " + OService.getTotalDailyRegister(coffe, date) + " y el número de pedidos ha sido: " + Oservice.getNumberOfDailyOrders(coffe,date);
+		return "La caja de la fecha introducida es: " + OService.getTotalDailyRegister(coffe, date) + " y el nï¿½mero de pedidos ha sido: " + OService.getNumberOfDailyOrders(coffe,date);
 	}
 
 }
