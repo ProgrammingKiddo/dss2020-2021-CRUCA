401fb597f77e244305505bd0d22bee8af872b636
FcoJoseBernalPerez <65712874+FcoJoseBernalPerez@users.noreply.github.com>
2021-05-13 09:55:44 +0200

    ApihttpApplication class fix

--- apihttp/src/main/java/apihttp/ApihttpApplication.java
+++ apihttp/src/main/java/apihttp/ApihttpApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = "coreapi")
 public class ApihttpApplication {
 
 	public static void main(String[] args) {
