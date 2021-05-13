0c3414d1fe8741a11ee780a1ef8fd6bf707b8590
María Vázquez Sáez <mariav.s.00@gmail.com>
2021-05-13 13:12:56 +0200

    Add method getUser and editProfile

--- apihttp/src/main/java/apihttp/UserController.java
+++ apihttp/src/main/java/apihttp/UserController.java
@@ -1,4 +1,5 @@
 package apihttp.src.main.java.apihttp;
+import filepersistence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,76 +10,19 @@ import api.src.main.java.coreapi.Order;
 import api.src.main.java.coreapi.Period;
 import api.src.main.java.coreapi.String;
 
-
-
 @RestController
 public class UserController 
 {
-	private final User user;
+	@GetMapping("/users/profile/{dni}")
+    User getUser(@PathVariable int dni)
+    {
+        return Load.LoadUser(dni);
+    }
+    
+    @PutMapping("/users/editProfile/{user}")
+    void editProfile(@RequestBody User user)
+    {
+        Save.saveUser(user);
+    }
 	
-	UserController(User user)
-	{
-		this.user = user;
-	}
-	
-	@GetMapping("/users")
-	int get_n_card()
-	{
-		return user.get_n_card();
-	}
-	
-	@GetMapping("/users")
-	string get_name()
-	{
-		return user.get_name();
-	}
-	
-	@GetMapping("/users")
-	Period get_age()
-	{
-		return user.get_age();
-	}
-	
-	@GetMapping("/users")
-	string get_surname()
-	{
-		return user.get_surname();
-	}
-	
-	@GetMapping("/users")
-	List<Order> get_UserOrderList()
-	{
-		return user.get_UserOrderList();
-	}
-	
-	@PatchMapping("/users/{parameters}")
-	void set_name(@PathVariable("parameters")String Name)
-	{
-		user.set_name(Name);
-	}
-	
-	@PatchMapping("/users/{parameters}")
-	void set_surname(@PathVariable("parameters")String Surname)
-	{
-		user.set_surname(Surname);
-	}
-	@PatchMapping("/users/{parameters}")
-	void set_n_card(@PathVariable("parameters")String N_card)
-	{
-		user.set_n_card(N_card);
-	}
-		
-	@PatchMapping("/users/{parameters}")
-	void set_BirthDate(@PathVariable("parameters")LocalDate newDate)
-	{
-		user.set_BirthDate(newDate);
-	}
-	
-	@PatchMapping("/users/scheduleOrder/{parameters}")
-	void scheduleOrder(@PathVariable("parameters")LocalDate date, LocalTime time, Order ord)
-	{
-		LocalDateTime scheduleDate = LocalDateTime.of(date,time);
-		//Asignar esta fecha y hora a la fecha y hora del pedido
-	}
-		
 }
