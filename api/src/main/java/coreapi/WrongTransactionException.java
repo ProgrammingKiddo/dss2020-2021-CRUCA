e4cbe2575bdbbb05404685615db2487e05a6a33c
FcoJoseBernalPerez <65712874+FcoJoseBernalPerez@users.noreply.github.com>
2021-06-01 19:01:31 +0200

    Exception of cards operations.

---
+++ api/src/main/java/coreapi/WrongTransactionException.java
@@ -0,0 +1,14 @@
+package coreapi;
+/**
+ * 
+ * @author María
+ * @author Fran
+ */
+
+public class WrongTransactionException extends Exception {
+
+	public WrongTransactionException(String message) {
+		super(message);
+	}
+
+}
