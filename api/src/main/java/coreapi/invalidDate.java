4f46a4143f8b261c7be2f1e370aec492d15df033
María Vázquez Sáez <mariav.s.00@gmail.com>
2021-06-05 01:03:26 +0200

    Creation of javadoc in the invalidDate expection class.

--- api/src/main/java/coreapi/invalidDate.java
+++ api/src/main/java/coreapi/invalidDate.java
@@ -1,6 +1,20 @@
 package coreapi;
 
+/**
+* This exception class represents the situation where a client is trying to add
+* a invalid date. 
+* @author María
+* @author Fran
+* @see OrderImpl
+* @see OrderService
+*/
+
 public class invalidDate extends Exception{
+	/**
+	* Create a new exception of this type, detailing the message.
+	* the details of the date you are trying to assign.
+	* @param message 	The message detailing the details of the problem.
+	*/
 	public invalidDate(String message)
 	{
 		super(message);
