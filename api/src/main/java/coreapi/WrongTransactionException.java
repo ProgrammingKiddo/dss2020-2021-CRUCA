88f80562f2ff91f7614bb939abd043e586139994
María Vázquez Sáez <mariav.s.00@gmail.com>
2021-06-05 10:09:50 +0200

    Add Javadoc for WrongTransaction Exception class.

--- api/src/main/java/coreapi/WrongTransactionException.java
+++ api/src/main/java/coreapi/WrongTransactionException.java
@@ -1,12 +1,19 @@
 package coreapi;
 /**
- * 
- * @author María
+ * This class of exception represents the situation in which a customer enters 
+ * incorrect data when he is going to carry out a transaction, 
+ * and therefore this cannot be carried out correctly.
+ * @author Marï¿½a
  * @author Fran
  */
 
 public class WrongTransactionException extends Exception {
 
+	/**
+	 * Create a new exception of this type, detailing on the message
+	 * exactly why the transaction failed.
+	 * @param message The message detailing the specifics of the issue.
+	 */
 	public WrongTransactionException(String message) {
 		super(message);
 	}
