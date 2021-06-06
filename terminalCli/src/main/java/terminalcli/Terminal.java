ccf0a78891cc94d6e0cb0fef130c027eefed0e00
ProgrammingKiddo <programming.edu@protonmail.com>
2021-06-06 18:27:09 +0200

    Modified the creation of base information to work with the new Data system.

--- terminalCli/src/main/java/terminalcli/Terminal.java
+++ terminalCli/src/main/java/terminalcli/Terminal.java
@@ -12,34 +12,37 @@ import coreapi.OrderFactory;
 import coreapi.OrderService;
 import coreapi.Product;
 import coreapi.ProductImpl;
-import filepersistence.Load;
-import filepersistence.Save;
-import coreapi.ProductCatalog;
+import data.CafeteriaData;
+import data.OrderData;
+import data.ProductData;
+import filepersistence.DiskCafeteriaData;
+import filepersistence.DiskOrderData;
+import filepersistence.DiskProductData;
 
 public class Terminal {
 
 	static boolean create;
 	static String path = "./";
 	static Cafeteria coffee;
+	static CafeteriaData coffeeData;
+	static OrderData orderData;
+	static ProductData productData;
+	static OrderService ordSer;
+	
+	
+	public static void main(String[] args)
+	{
+		if (args.length != 0) path = args[0];
 
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		terminalParameters(args);
-		System.out.println(path);
-		if (create == true)
-		{
-			createObjects();
-		}
-		else
-		{
-			loadObjects();
-		}
-		Screen terminal = new Screen();
-		terminal.main_screen(coffee);
+		coffeeData = new DiskCafeteriaData(path);
+		orderData = new DiskOrderData(path);
+		productData = new DiskProductData(path);
+		ordSer = new OrderService(coffeeData, orderData, productData);
 		
-		saveObjects();
+		
+		createObjects();
+
+		Screen terminal = new Screen();
 	}
 
 	
@@ -63,69 +66,55 @@ public class Terminal {
 	}
 	
 	private static void createObjects()
-	{
-		OrderService ordSer = new OrderService();
-		coffee = new Cafeteria(0, "Cafeteria ESI");
+	{		
+		coffee = new Cafeteria(0, "Cafeteria ESI", "cafeteria@esi.uca.es");
 		coffee.AddType("Menu");
 		coffee.AddType("Comida");
 		coffee.AddType("Bebida");
 		
-		ProductCatalog.Instance().addProduct(new ProductImpl(1, new BigDecimal(1.7), "Bacon-queso-huevo", "Menu"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(0, new BigDecimal(1.2), "Patatas fritas", "Comida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(2, new BigDecimal(0.9), "Café con leche", "Bebida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(3, new BigDecimal(0.5), "Doritos", "Comida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(4, new BigDecimal(1.6), "Monster", "Bebida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(5, new BigDecimal(1.3), "Bocadillo de tortilla", "Comida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(6, new BigDecimal(0.8), "Botella de agua", "Bebida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(7, new BigDecimal(0.7), "Donut blanco", "Comida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(8, new BigDecimal(0.75), "Donut de chocolate", "Comida"));
-		ProductCatalog.Instance().addProduct(new ProductImpl(9, new BigDecimal(1.4), "Sándwich de roquefort", "Menu"));
+		List<Product> productList = new ArrayList<Product>();
 		
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(0), 30);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(1), 10);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(2), 5);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(3), 15);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(4), 20);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(5), 50);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(6), 10);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(7), 8);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(8), 12);
-		coffee.registerProduct(ProductCatalog.Instance().getProduct(9), 20);
+		productList.add(new ProductImpl(0, new BigDecimal(1.2), "Patatas fritas", "Comida"));
+		productList.add(new ProductImpl(1, new BigDecimal(1.7), "Bacon-queso-huevo", "Menu"));
+		productList.add(new ProductImpl(2, new BigDecimal(0.9), "Café con leche", "Bebida"));
+		productList.add(new ProductImpl(3, new BigDecimal(0.5), "Doritos", "Comida"));
+		productList.add(new ProductImpl(4, new BigDecimal(1.6), "Monster", "Bebida"));
+		productList.add(new ProductImpl(5, new BigDecimal(1.3), "Bocadillo de tortilla", "Comida"));
+		productList.add(new ProductImpl(6, new BigDecimal(0.8), "Botella de agua", "Bebida"));
+		productList.add(new ProductImpl(7, new BigDecimal(0.7), "Donut blanco", "Comida"));
+		productList.add(new ProductImpl(8, new BigDecimal(0.75), "Donut de chocolate", "Comida"));
+		productList.add(new ProductImpl(9, new BigDecimal(1.4), "Sándwich de roquefort", "Menu"));
+
+		for (Product prod : productList)
+		{
+			productData.saveProduct(prod);
+			coffee.registerProduct(prod, 20);
+		}
+		
 		
 		Order order1 = OrderFactory.createOrder(coffee);
 		Order order2 = OrderFactory.createOrder(coffee);
 		Order order3 = OrderFactory.createOrder(coffee);
 		Order order4 = OrderFactory.createOrder(coffee, LocalDateTime.of(2021, 2, 23, 14, 0));
 		try {
-			ordSer.addProductToOrder(coffee, order1, 0, 5);
-			ordSer.addProductToOrder(coffee, order1, 1, 1);
-			ordSer.addProductToOrder(coffee, order1, 9, 7);
-			ordSer.addProductToOrder(coffee, order2, 5, 15);
-			ordSer.addProductToOrder(coffee, order3, 3, 4);
-			ordSer.addProductToOrder(coffee, order3, 3, 5);
-			ordSer.addProductToOrder(coffee, order4, 7, 2);
+			ordSer.addProductToOrder(coffee, order1, productList.get(0), 5);
+			ordSer.addProductToOrder(coffee, order1, productList.get(1), 1);
+			ordSer.addProductToOrder(coffee, order1, productList.get(9), 7);
+			ordSer.addProductToOrder(coffee, order2, productList.get(5), 15);
+			ordSer.addProductToOrder(coffee, order3, productList.get(3), 4);
+			ordSer.addProductToOrder(coffee, order3, productList.get(3), 5);
+			ordSer.addProductToOrder(coffee, order4, productList.get(7), 2);
 		} catch (InsufficientStockException ex)
 		{
 			//...
 		}
 		
+		orderData.saveOrder(order1);
+		orderData.saveOrder(order2);
+		orderData.saveOrder(order3);
+		orderData.saveOrder(order4);
+		
+		coffeeData.saveCafeteria(coffee);
 	}
 	
-	private static void loadObjects()
-	{
-		System.out.println("Loading objects...");
-		Load fileLoader = new Load(path);
-		fileLoader.LoadProducts();
-		System.out.println("Objects loaded!");
-		fileLoader.LoadCafeteria(coffee);
-		fileLoader.LoadOrders(coffee);
-	}
-	
-	private static void saveObjects()
-	{
-		Save fileSaver = new Save(path);
-		fileSaver.SaveProducts();
-		fileSaver.SaveCafeteria(coffee);
-		fileSaver.SaveOrders(coffee);
-	}
 }
