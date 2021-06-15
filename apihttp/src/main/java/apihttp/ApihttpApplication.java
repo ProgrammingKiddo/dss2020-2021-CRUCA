cfa762d9b781750e3a1c38efbaf0ce3a67904511
María Vázquez Sáez <mariav.s.00@gmail.com>
2021-06-15 11:26:33 +0200

    Swagger dependency

--- apihttp/src/main/java/apihttp/ApihttpApplication.java
+++ apihttp/src/main/java/apihttp/ApihttpApplication.java
@@ -11,10 +11,12 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Configuration
 @RestController
+@EnableSwagger2
 @ComponentScan(basePackages = "coreapi")
 public class ApihttpApplication {
 
